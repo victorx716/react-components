dae2b53e91b600d70bd9b6de0a741a241b3da696
victorx716 <victorliu716@gmail.com>
2019-01-16 14:39:31 -0800

    Added some basic refactoring

--- app.js
+++ app.js
@@ -1,1 +1,33 @@
 // TODO
+class GroceryList extends React.Component {
+    
+    constructor(props) {
+      super(props);
+    }
+    
+    render() {
+      
+      return (
+          <li>{this.props.items}</li>
+      );
+    }
+}
+
+
+
+var GroceryListItem = (props) => (
+  <ul>
+    {props.items.map(items =>
+      <GroceryListItem items={items} />
+      )}
+  </ul>
+);
+
+ReactDOM.render(<GroceryListItem />, document.getElementById('app'));
+
+
+// /*
+//     <h2> My Shopping List </h2>
+//     <GroceryList items = {['Meatballs', 'Kale']}/>
+
+// */
